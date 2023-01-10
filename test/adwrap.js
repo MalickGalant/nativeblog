08aeffc5d0d880a5c10adcd27460e4417cf1983e
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2023-01-10 21:41:05 +0200

    Create adwrap.js

---
+++ test/adwrap.js
@@ -0,0 +1,10 @@
+import { ads, H5AdWrapper } from '../src/h5-ad-wrapper'
+
+/**
+ * AdWrapper test
+ */
+describe('ads isntance', () => {
+    it('', () => {
+        expect(ads).toBeInstanceOf(H5AdWrapper)
+    })
+})
